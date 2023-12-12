c73005b58c086dc7a0898ca343ed40c70d4142e4
Tejas Bobhate <tejas@simplified.co>
2023-12-11 21:26:03 -0800

    one more silly try

--- demoTalkNet.py
+++ demoTalkNet.py
@@ -758,7 +758,7 @@ def main():
 
     print("source_fps : ", source_fps)
     # Extract video
-    args.videoFilePath = os.path.join(args.pyaviPath, "video_01.avi")
+    args.videoFilePath = os.path.join(args.pyaviPath, "video_01.mp4")
     # If duration did not set, extract the whole video, otherwise extract the video from 'args.start' to 'args.start + args.duration'
     if args.duration == 0:
         command = (
